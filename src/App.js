11ce107a60d50db789845dcbd651a0159a95f3f3
hfcheung <hfcheung@uw.edu>
2022-01-23 23:39:32 -0800

    Added initial files, Chatter App initial screen rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // no logo on site in order to make chatter app look cleaner
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <span className="title">CHATTER APP</span>
       </header>
+      <footer className="footer">
+        <input className="text-input"/>
+        <button className="send-button">↑</button>
+      </footer>
     </div>
   );
 }
